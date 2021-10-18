cd979f54a44d05719e72316c41b90012d969b9d1
dlfrnaos19 <dlfrnaos19@gmail.com>
2021-10-18 20:01:08 +0900

    전체 소스코드

--- AutoConnect.py
+++ AutoConnect.py
@@ -1,16 +1,18 @@
 from pywinauto import application
-import os, time, json
-with open('info.json') as f:
+import time
+import os
+import json
+with open(os.path.join(os.getcwd(), 'info.json'),'r',encoding='utf8') as f:
     info = json.load(f)
+    
 os.system('taskkill /IM coStarter* /F /T')
 os.system('taskkill /IM CpStart* /F /T')
 os.system('taskkill /IM DibServer* /F /T')
-os.system('wmic process where "name like \"%coStarter%\'" call terminate')
-os.system('wmic process where "name like \"%CpStart%\'" call terminate')
-os.system('wmic process where "name like \"%DibServer%\'" call terminate')
+os.system('wmic process where "name like \'%coStarter%\'" call terminate')
+os.system('wmic process where "name like \'%CpStart%\'" call terminate')
+os.system('wmic process where "name like \'%DibServer%\'" call terminate')
+time.sleep(5)        
 
-time.sleep(5)
 app = application.Application()
-app.start('C:\CREON\STARTER\coStarter.exe /prj:cp '
-          '/id:{} /pwd:{} /pwdcert:{} /autostart'.format(info['id'], info['pwd'], info['pwdcert']))
-time.sleep(30)
+app.start(f'''C:\CREON\STARTER\coStarter.exe /prj:cp /id:{info['id']} /pwd:{info['password']} /pwdcert:{info['certpassword']} /autostart')''')
+time.sleep(60)
